c5d9c3be99fa661a0f23fa2fa052cf422b045467
gsommers <gsommers@princeton.edu>
2017-07-12 11:33:33 -0400

    7/12/17

---
+++ Window.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// handles panels that can be hidden and displayed
+public class Window : MonoBehaviour {
+
+    // called by open and close buttons
+    public void SetVisibility(string button)
+    {
+        if (button.CompareTo("open") == 0)
+            gameObject.SetActive(true); // show window
+        else if (button.CompareTo("close") == 0)
+            gameObject.SetActive(false); // close window
+    }
+}
